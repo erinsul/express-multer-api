7d4462afce37d6b1f5c2070f3ce2ec3ba0ba775a
erinsul <erin_sullivan50@yahoo.com>
2016-07-08 15:28:58 -0400

    Refactor into upload module
    This module is usable from both a command-line application and an express api
    server.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,36 +1,8 @@
 'use strict';
 
 require('dotenv').config();
-
-const crypto = require('crypto');
-
 const fs = require('fs');
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  }
-});
-
-const mimeType = (buffer) => {
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream',
-  }, fileType(buffer));
-};
-
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error){
-        reject(error);
-      }
-      resolve(buffer.toString('hex'));
-    });
-  });
-};
+const uploader = require('../lib/aws-s3-upload');
 
 let filename = process.argv[2] || '';
 
@@ -45,36 +17,8 @@ const readFile = (filename) => {
   });
 };
 
-const awsUpload = (file) => {
-  return randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-    return {
-      ACL: "public-read",
-      Body: file.data,
-      Bucket: 'erin-wdi-files',
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`
-    };
-  })
-  .then((options) => {
-    return new Promise((resolve, reject) => {
-      s3.upload(options, (err, data) => {
-        if(err) {
-          reject(err);
-        }
-        resolve(data);
-      });
-    });
-  });
-};
-
 readFile(filename)
-.then((data) => {
-  let file = mimeType(data);
-  file.data = data;
-  return file;
-})
-.then(awsUpload)
+.then(uploader.prepFile)
+.then(uploader.awsUpload)
 .then(console.log)
 .catch(console.error);
